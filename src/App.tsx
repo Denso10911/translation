45e7412be758d99f0c56a571cb5d8848a97d343d
denso10911 <denso10911@gmail.com>
2024-03-14 16:55:16 +0200

    Change global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import 'regenerator-runtime/runtime'
 import Dictaphone from "./components/Dictaphone";
+import {DarkMode} from "@chakra-ui/react";
 
 function App() {
   
   return (
-    <>
+    <DarkMode>
       <Dictaphone />      
-    </>
+    </DarkMode>
   )
 }
 
